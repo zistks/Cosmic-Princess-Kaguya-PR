4ce0b92653d35b7364cdff677a6806c367fc5542
zistks <zist0207@gmail.com>
2026-02-04 01:55:12 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
     localStoragePrefix: "party-rank-sorter",
-    title: "Party Rank Sorter",
-    description: "Party rank sorter for your custom list of songs."
+    title: "Cosmic Princess Kaguya PR",
+    description: "Party rank sorter for Cosimc Princess Kaguya."
 };
